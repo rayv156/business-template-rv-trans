d511e671539c7e50a58ffa2357e2ea7b54fb554e
Ray Velasquez <rayv156@gmail.com>
2022-10-04 20:27:56 -0500

    added pink logo

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import companyLogo from '../../src/images/rv_trans_logo.png'
+import companyLogo from '../../src/images/pink_logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRibbon } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,7 +8,7 @@ const Hero = () => {
       <section className="hero is-medium" >
             <div className="hero-body">
                 <img className="company-logo" src={companyLogo} alt="RV Trans Logo"/>
-                <FontAwesomeIcon icon={faRibbon} style={{fontSize: "200px" ,color: 'hotpink', position: 'absolute', zIndex: '-1'}} size="lg"/>
+                <FontAwesomeIcon icon={faRibbon} style={{fontSize: "120px" ,color: 'rgb(240, 0, 186)', position: 'absolute', zIndex: '2', margin: '100px 0 0 140px'}} size="lg"/>
             </div>
       </section>
   </div>;
